f029a3163d8cd82e09ac2e96d79d86b91c19df5b
hkhattar <himanshikhatter@gmail.com>
2016-11-24 15:50:50 -0800

    after order by likes

--- client/assets/controllers/belt_controller.js
+++ client/assets/controllers/belt_controller.js
@@ -7,28 +7,29 @@ app.controller('belt_controller', ['$scope','$cookies','$location','$routeParams
       var answer = {};
       $scope.error = {};
 
-      function setQuestions(data){
+      function setQuestions(data)
+      {
         $scope.questions = data;
-        // $scope.question = {};
       }
 
-      function setAnswers(data){
+      function setAnswers(data)
+      {
         $scope.answers = data;
         $scope.answer = {};
       }
       $scope.answer = {};
-      function setAnswer(data){
-        $scope.answer = data;
 
+      function setAnswer(data)
+      {
+        $scope.answer = data;
       }
       
       console.log('belt_controller loaded');
 
-
       $scope.create_new_user = function()
       {
-
-        if($scope.new_user.length > 5){
+        if($scope.new_user.length > 5)
+        {
           $scope.error = {};
           belt_factory.set_user($scope.new_user, function(data){
           $scope.current_user = data;
@@ -40,80 +41,79 @@ app.controller('belt_controller', ['$scope','$cookies','$location','$routeParams
 
       }
 
-      belt_factory.get_user(function(data){
+      // $scope.create_question = function()
+      // {
+      //  if($scope.newQuestion.question.length > 10)
+      //   {
+      //     $scope.error = {};
+      //     belt_factory.set_question($scope.newQusetion.question, function(data){
+      //     $scope.current_question = data;
+      //     $location.url('/');
+      //   })
+      //   }else{
+      //     $scope.error = {message: 'Your name must be at least 6 characters long!'};
+      //   }
+      // }
+
+      belt_factory.get_user(function(data)
+      {
         $scope.current_user = data;
-   
-       })
+      })
       
-       // $scope.create_user = function()
-
-       //  {
-           
-       //    belt_factory.create_user($scope.newUser)
-       //    $location.url('/');
-       //  }
-
-       $scope.getUser = function(){
+      $scope.getUser = function()
+      {
         user = belt_factory.getUser();
         return user;
-       }
+      }
 
-       // belt_factory.get_user(function(data){
-       //  $scope.user = data;
-       // })
+      $scope.cancel=function()
+      {
+        $location.url('/');
+      }
 
-        $scope.cancel=function(){
-          $location.url('/');
-        }
-
-       $scope.create_question = function()
-       {
-          console.log('create_question function of belt_controller')
-          console.log('$scope.newQuestion',$scope.newQuestion)
-          // var new_friend = $scope.newFriend;
+      $scope.create_question = function()
+      {
+        if($scope.newQuestion.question.length > 10)
+        {
+          $scope.error = {};
           belt_factory.create_question($scope.newQuestion,setQuestions);
           $scope.newQuestion = {};
-          // /$scope.friends.push($scope.newFriend);
-          console.log("$scope.questions",$scope.questions)
-          // console.log("$scope.friends[0].name",$scope.friends[0].name)
-          console.log('create_question function after user creates a question')
           $location.url('/');
+        }
+        else{
+          $scope.error = {message: 'Your question must be at least 10 characters long!'};
+        }
+     
       }
 
       $scope.create_answer_by_id = function()
       {
-        console.log('create_answer function of belt controller');
-        var user = belt_factory.getUser();
-        console.log('user',user);
-        console.log('$routeParams.id',$routeParams.id);
-        console.log('$scope.newAnswer',$scope.newAnswer);
-        belt_factory.create_answer_by_id($routeParams.id,$scope.newAnswer,user,function(data){
-          console.log('data',data);
-          $scope.answer = data;
-          console.log('$scope.answer',$scope.answer)
-          setAnswer(data);
-          // return data;
+        if($scope.newAnswer.answer.length > 5)
+        {
+          $scope.error = {};
+          var user = belt_factory.getUser();
+          belt_factory.create_answer_by_id($routeParams.id,$scope.newAnswer,user,function(data)
+          {
+            $scope.answer = data;
+            setAnswer(data);
           })
-        $location.url('/')
-        console.log("add location.url over here..............")
-
-
+          $location.url('/')
+        }
+        else{
+          $scope.error = {message: 'Your answer must be at least 5 characters long!'};
+        }
       }
-
 
       $scope.show_user = function()
       {
-      	console.log('show function of belt_controller');
       	belt_factory.show($scope.user, function(data)
         {
       	 $scope.user = data;
-      	 console.log('$scope.user')
       	})
       }
 
       $scope.index_question = function(){
         belt_factory.index_question(function(data){
-          console.log('DATA',data)
           $scope.questions = data;
           $scope.question = {};
         })
@@ -121,31 +121,29 @@ app.controller('belt_controller', ['$scope','$cookies','$location','$routeParams
 
       $scope.index_question();
 
-      $scope.get_question_by_id = function(){
+      $scope.get_question_by_id = function()
+      {
         console.log('controller--get_question_by_id');
-        belt_factory.get_question_by_id($routeParams.id,function(data){
-          console.log('routeParams.id',$routeParams.id);
-          console.log('set data',data, 'to $routeParams.id',$routeParams.id);
+        belt_factory.get_question_by_id($routeParams.id,function(data)
+        {
           $scope.question = data;
         })
       }
 
-      $scope.plusLike = function(answerId,likes){
+      $scope.plusLike = function(answerId,likes)
+      {
         var increment = parseInt(likes) + 1;
-
-        belt_factory.updateAnswer(answerId,increment,function(data){
+        belt_factory.updateAnswer(answerId,increment,function(data)
+        {
           console.log('payload',data);
           var url = '/';
           console.log('redirect to /');
           $scope.get_question_by_id();
         });
       }
-
       $scope.get_question_by_id();
 
     }
 
-
-
-
 ]);
+
--- client/assets/factories/belt_factory.js
+++ client/assets/factories/belt_factory.js
@@ -35,25 +35,21 @@ app.factory('belt_factory',function($http){
     callback(current_user);
   }
 
-        factory.create_question = function(newQuestion,callback){
-        	console.log('create_question method of the belt_factory');
-        	
-        	console.log('newQuestion',newQuestion);
-        	$http.post('/questions', newQuestion).then(function(returned_data){
-        		console.log("returned_data.data",returned_data.data);
-            console.log("*************************s")
+  // factory.set_question = function(question,callback){
+  //   current_question = question;
+  //   callback(current_question);
+  // }
 
-            if (typeof(callback) == 'function'){
-                      console.log('end of create_question method of factory');
-                      console.log('returned_data.data',returned_data.data);
-                      callback(returned_data.data);
-          }
-
-
-        	});
-         	
-
-       }
+  factory.create_question = function(newQuestion,callback)
+  {
+    $http.post('/questions', newQuestion).then(function(returned_data)
+    {
+      if (typeof(callback) == 'function')
+      {
+        callback(returned_data.data);
+      }
+    });
+  }
 
        factory.index_question = function(callback){
         console.log('factory index_question')
@@ -79,7 +75,7 @@ app.factory('belt_factory',function($http){
         console.log('create_answer_by_id in factory');
         console.log('answerContent',answerContent)
         console.log('user',user)
-        $http.post('/answers/' + id,{name: user.name, answer: answerContent}).then(function(returned_data){
+        $http.post('/answers/' + id,{name: user, answer: answerContent}).then(function(returned_data){
           console.log('******returned_data.data*****',returned_data.data)
           console.log('inside factory.create_answer_by_id');
           if(typeof(callback) == 'function'){
